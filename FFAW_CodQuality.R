afd1a86d54d4028c5b9a25e743ebbd7bbe62dd3b
jchawarski <julian.chawarski@gmail.com>
2021-03-02 22:15:59 -0330

    fskduhfkds

--- FFAW_CodQuality.R
+++ FFAW_CodQuality.R
@@ -122,11 +122,27 @@ cod.dat$Index <- c(1:length(cod.dat$Date))
 
 time.dat <- time.dat[complete.cases(time.dat), ] # Store the complete cases subset in a new data frame
 
-cod.sub <- cod.dat %>% select(Index, HLogId, UNQID_HARVESTER, GTName, Reefer, Ice, IceType, LiveBleed, ProdWgt, Grade, GradeBP)
+cod.sub <- cod.dat %>% 
+  select(Index, 
+         Site,
+         HLogId, 
+         UNQID_HARVESTER,
+         UNQID_GRADERS,
+         GTName, 
+         Reefer, 
+         Ice, 
+         IceType, 
+         LiveBleed, 
+         ProdWgt, 
+         Grade, 
+         GradeBP)
 
 time.sub <- time.dat %>% left_join(., cod.sub, by="Index")
 time.sub$GTName <- trimws(time.sub$GTName)
-time.sub <- time.sub %>% mutate(GTName = recode(GTName, "HOOK AND LINE" = "HANDLINE"))
+time.sub <- time.sub %>% mutate(GTName = recode(GTName, "HOOK AND LINE" = "HANDLINE")) %>% 
+                  filter(!Grade %in% c("NULL", "R"))
+
+
 
 #Example timeline
 time.dat  <- time.dat[,c(18:29)]
@@ -141,7 +157,7 @@ time.melt %>% filter(Index %in% 1200) %>% ggplot(., aes(x=value, y=0, color=vari
   geom_text(aes(x = value, y = dislocations, label=variable), angle = 45, position="jitter") + ylim(-2,2) + 
   
   theme_minimal(base_size=14) +
- theme(legend.position="none", 
+  theme(legend.position="none", 
        axis.text.y = element_blank(),
        axis.ticks.y = element_blank(),
        axis.title.y = element_blank())
@@ -150,32 +166,115 @@ time.melt %>% filter(Index %in% 1200) %>% ggplot(., aes(x=value, y=0, color=vari
 
 # look at variability of timeline in CoC
 
+
+#Soak time - how long the gear was in the water
+time.sub$Soak <- as.numeric(time.sub$Haul_DT - time.sub$Set_DT)/60 # calculated in minutes
+time.sub %>% filter(between(Soak, 0, 10000)) %>%
+  ggplot(., aes(x=Grade, y=Soak/60)) + 
+  geom_boxplot() +  ylab("Soak Time [hours]") +
+  theme_minimal(base_size=14) + facet_grid(~GTName)
+
+
 #Transport time - from Pickup to Delivery
 time.sub$Transport <-  as.numeric(time.sub$Del_DT - time.sub$PU_DT)/60
-time.sub <- time.sub %>% filter(Transport > 0 & Transport < 10000)
+time.sub %>% filter(between(Transport, 0, 10000)) %>%
+ggplot(., aes(x=Grade, y=Transport/60)) + 
+  geom_boxplot() +  ylab("Transport [hours]") +
+  theme_minimal(base_size=14) + facet_grid(~GTName)
+
+
 #Process wait time - from Delivery to Processing
 time.sub$Factory <- as.numeric(time.sub$ProcStart_DT - time.sub$Del_DT)/60 # calculated in minutes
-time.sub <- time.sub %>% filter(Factory > 0 & Factory < 10000)
+time.sub %>% filter(Factory > 0 & Factory < 10000) %>%
+ggplot(., aes(x=Grade, y=Factory/60)) + 
+  geom_boxplot() +  ylab("Factory Wait [hours]") +
+  theme_minimal(base_size=14) + facet_grid(~GTName)
+
+
 #Dock Sitting time - time from Unload to Pickup
 time.sub$Dock <- as.numeric(time.sub$PU_DT - time.sub$UnloadEnd_DT)/60 # calculated in minutes
-time.sub <- time.sub %>% filter(Dock > 0 & Dock < 10000)
-
-
-
-ggplot(time.sub, aes(x=Grade, y=Transport)) + 
-  geom_boxplot() +  
-  theme_minimal(base_size=14) + facet_grid(~GTName)
-
-ggplot(time.sub, aes(x=Grade, y=Factory)) + 
-  geom_boxplot() +  
-  theme_minimal(base_size=14) + facet_grid(~GTName)
-
-ggplot(time.sub, aes(x=Grade, y=Dock)) + 
-  geom_boxplot() +  
+time.sub %>% filter(Dock > 0 & Dock < 10000) %>%
+ggplot(., aes(x=Grade, y=Dock/60)) + 
+  geom_boxplot() +  ylab("Dock Wait [hours]") +
   theme_minimal(base_size=14) + facet_grid(~GTName)
 
 
 
-cod.sum <- cod.sub %>% group_by(HLogId) %>% summarize(wwt = unique(ProdWgt),
-                                                      pgb = unique(GradeBP))
+# summarize the percentage of each grade
+cod.sum <- cod.sub %>% 
+group_by(HLogId, Grade) %>%
+  summarise(n = n(), 
+            wwt = unique(ProdWgt)) %>%
+  mutate(freq = n / sum(n))
 
+# plot the percentage of grade B against the Total Weight of Catch
+cod.sum %>% 
+  filter(Grade %in% "B") %>% 
+  ggplot(., aes(x=freq, y=as.numeric(wwt))) + 
+  geom_point()
+
+#distributions
+hist(cod.sum$freq, breaks= 10) 
+hist(as.numeric(cod.sum$wwt), breaks =100)
+
+
+# quick look at the effects of harvesters on grade
+cod.sum <- cod.sub %>% 
+  group_by(HLogId, Grade) %>%
+  summarise(n = n(), 
+            wwt = unique(ProdWgt), 
+            site = unique(Site),
+            harv = unique(UNQID_HARVESTER),
+            grader = unique(UNQID_GRADERS)) %>%
+              mutate(freq = n / sum(n))
+
+H_102 <- cod.sum %>% filter(Grade %in% "A") %>% 
+  filter(harv %in% "H-102") %>% filter(freq < 1)
+H_204 <- cod.sum %>% filter(harv %in% "H-204")
+H_104 <- cod.sum %>% filter(harv %in% "H-104")
+
+
+# site by harvester and their ranking by grade - this is likely an incorrect calculation of their stats
+  cod.sum %>% filter(Grade %in% "A") %>% filter(freq < 1) %>%
+  ggplot(., aes(x=harv, y=site, fill=freq)) + geom_tile(stat="identity") +
+  scale_fill_gradient(low = "red", high = "yellow", na.value = NA) +
+  theme_minimal(base_size=8) + theme(axis.text.x = element_text(angle=90)) 
+
+  
+# create a new df with each harvesters downgraded score as the mean  
+ harv.sum <- cod.sum %>% 
+   group_by(harv, site) %>%
+   filter(Grade %in% "A") %>%
+   summarize(dwngrd = 1 - mean(freq))
+                                                      
+ ggplot(harv.sum, aes(x=harv, y=site, fill=dwngrd*100)) + geom_tile(stat="identity") +
+   scale_fill_viridis_c(name = "Percent Downgraded")  +
+   theme_minimal(base_size=8) + theme(axis.text.x = element_text(angle=90),
+                                      legend.position = "top")
+
+ 
+#total downgrades by region
+ 
+ cod.dat$GTName <- trimws(cod.dat$GTName)
+ 
+site.grd <- cod.dat %>% group_by(Site, Grade) %>% filter(!Grade %in% "NULL" ) %>% 
+  filter(GTName == "NETS") %>%
+  summarise(n = n()) %>% 
+   mutate(freq = n / sum(n)) %>%
+    drop_na(freq) %>%
+    filter(Grade %in% "A") 
+ 
+ggplot(site.grd, aes(x=reorder(Site, -freq), y=(1-freq)*100)) + 
+  geom_bar(stat="identity", fill="navyblue")  + coord_flip() + ylab("Percent Downgraded") + xlab("Site") +
+  theme_minimal(base_size=10) 
+#grader effects
+ 
+ 
+ unique(cod.dat$Comments2)
+                                          
+ harv.sum <- cod.sum %>% 
+   group_by() %>%
+   filter(Grade %in% "A") %>%
+   summarize(dwngrd = 1 - mean(freq))
+ 
+ 
